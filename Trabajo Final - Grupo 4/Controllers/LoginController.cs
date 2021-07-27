15e3d7ace8cffdfd2f4f8ce58d8cfc91aebccb85
Alberto Bossio <alberto.federico.bossio@gmail.com>
2021-07-26 22:40:01 -0300

    asd

---
+++ Trabajo Final - Grupo 4/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Trabajo_Final___Grupo_4.Models;
+
+namespace Trabajo_Final___Grupo_4.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
