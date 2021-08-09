288c36b4924a85d1ab5a908912c59180d6aee4b6
Alberto Bossio <alberto.federico.bossio@gmail.com>
2021-08-08 22:09:49 -0300

    asd

---
+++ TPFinalGrupo4/Controllers/AlojamientoesController.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TPFinalGrupo4.Data;
+using System.Media;
+
+namespace TPFinalGrupo4.Models
+{
+    [Authorize]
+    public class AlojamientoesController : Controller
+    {
+        private readonly UsuarioContext _context;
+        private SoundPlayer _soundPlayer;
+
+        public AlojamientoesController(UsuarioContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Alojamientoes
+        public async Task<IActionResult> Index()
+        {
+            var usuarioLogeado = User.Identity;
+            return View(await _context.Alojamiento.ToListAsync());
+        }
+
+        // GET: Alojamientoes
+        public async Task<IActionResult> all(int? precio, String? estrellas, String? cantidadDePersonas)
+        {
+            var alojamientos = from alojamiento in this._context.Alojamiento
+                               select alojamiento;
+       
+            if (precio != null && estrellas != null && cantidadDePersonas != null)
+            {
+                alojamientos = alojamientos
+                    .Where(al => al.PrecioPorDia >= precio || al.PrecioPorPersona >= precio)
+                    .Where(al => al.Estrellas >= int.Parse(estrellas))
+                    .Where(al => al.CantidadDePersonas >= int.Parse(cantidadDePersonas));
+            }
+       
+            return View(alojamientos.ToList());
+        }
+
+        // GET: Alojamientoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var alojamiento = await _context.Alojamiento
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (alojamiento == null)
+            {
+                return NotFound();
+            }
+
+            return View(alojamiento);
+        }
+
+        // GET: Alojamientoes/Create
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Alojamientoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Codigo,Ciudad,Barrio,Estrellas,CantidadDePersonas,Tv,Tipo,PrecioPorPersona,PrecioPorDia,Habitaciones,Banios")] Alojamiento alojamiento)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(alojamiento);
+                await _context.SaveChangesAsync();
+                _soundPlayer = new SoundPlayer("Resources/SuccessSound.wav");
+                _soundPlayer.Play();
+                return RedirectToAction(nameof(Index));
+            }
+            _soundPlayer = new SoundPlayer("Resources/ErrorSound.wav");
+            _soundPlayer.Play();
+            return View(alojamiento);
+        }
+
+        // GET: Alojamientoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var alojamiento = await _context.Alojamiento.FindAsync(id);
+            if (alojamiento == null)
+            {
+                return NotFound();
+            }
+            return View(alojamiento);
+        }
+
+        // POST: Alojamientoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Codigo,Ciudad,Barrio,Estrellas,CantidadDePersonas,Tv,Tipo,PrecioPorPersona,PrecioPorDia,Habitaciones,Banios")] Alojamiento alojamiento)
+        {
+            if (id != alojamiento.Id)
+            {
+                _soundPlayer = new SoundPlayer("Resources/ErrorSound.wav");
+                _soundPlayer.Play();
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(alojamiento);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AlojamientoExists(alojamiento.Id))
+                    {
+                        _soundPlayer = new SoundPlayer("Resources/ErrorSound.wav");
+                        _soundPlayer.Play();
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                _soundPlayer = new SoundPlayer("Resources/SuccessSound.wav");
+                _soundPlayer.Play();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(alojamiento);
+        }
+
+        // GET: Alojamientoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var alojamiento = await _context.Alojamiento
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (alojamiento == null)
+            {
+                return NotFound();
+            }
+
+            return View(alojamiento);
+        }
+
+        // POST: Alojamientoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var alojamiento = await _context.Alojamiento.FindAsync(id);
+            _context.Alojamiento.Remove(alojamiento);
+            await _context.SaveChangesAsync();
+            _soundPlayer = new SoundPlayer("Resources/DeleteSound.wav");
+            _soundPlayer.Play();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AlojamientoExists(int id)
+        {
+            return _context.Alojamiento.Any(e => e.Id == id);
+        }
+
+        // GET: Alojamientoes/Edit/Buscar
+        [Authorize]
+        public async Task<IActionResult> Buscador(String searchCiudad, String searchTipo)
+        {
+            var alojamiento = from a in _context.Alojamiento select a;
+
+            if (!String.IsNullOrEmpty(searchCiudad))
+            {
+                var CiudadElejida = this._context.Ciudad.FirstOrDefault(nombreCiudad => nombreCiudad.Nombre == searchCiudad);
+                Console.WriteLine(CiudadElejida);
+                //int.Parse(searchCiudad);
+                alojamiento = alojamiento.Where(a => a.Ciudad.Contains(CiudadElejida.Codigo));
+            }
+
+            if (!String.IsNullOrEmpty(searchTipo))
+            {
+                alojamiento = alojamiento.Where(a => a.Tipo.Contains(searchTipo));
+            }
+
+            return View(await alojamiento.ToListAsync());
+        }
+
+    }
+}
---
+++ TPFinalGrupo4/Controllers/ReservasController.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TPFinalGrupo4.Data;
+using System.Media;
+
+namespace TPFinalGrupo4.Models
+{
+    [Authorize]
+    public class ReservasController : Controller
+    {
+        private readonly UsuarioContext _context;
+        private SoundPlayer _soundPlayer;
+
+        public ReservasController(UsuarioContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Reservas
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Reserva.Include(r => r.Alojamiento).Include(r => r.Usuario).ToListAsync());
+        }
+
+        // GET: Reservas
+        public async Task<IActionResult> List()
+        {
+            var reservas = from reserva in this._context.Reserva.Include(r => r.Alojamiento) select reserva;
+
+            reservas = reservas.Where(reserva => reserva.Usuario.Id == Int32.Parse(@User.Identity.Name));
+
+            return View(reservas.ToList());
+        }
+
+        // GET: Reservas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reserva = await _context.Reserva.Include(r => r.Alojamiento).Include(r => r.Usuario)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (reserva == null)
+            {
+                return NotFound();
+            }
+
+            return View(reserva);
+        }
+
+        // GET: Reservas/Create
+        [HttpGet]
+        public IActionResult Create(int? id, String? message)
+        {
+            if (id == null) return NotFound();
+            var alojamiento = this._context.Alojamiento.FirstOrDefaultAsync(alojamiento => alojamiento.Id == id).Result;
+            if (alojamiento == null) return NotFound();
+
+            ViewData["message"] = null;
+            if (message != null)
+                ViewData["message"] = message.Replace("-", " ");
+
+            ViewData["alojamiento_id"] = alojamiento.Id;
+            ViewData["alojamiento_ciudad"] = alojamiento.Ciudad;
+            ViewData["alojamiento_barrio"] = alojamiento.Barrio;
+            ViewData["alojamiento_estrellas"] = alojamiento.Estrellas;
+            ViewData["alojamiento_cantidadDePersonas"] = alojamiento.CantidadDePersonas;
+            ViewData["alojamiento_tv"] = alojamiento.Tv;
+            ViewData["alojamiento_tipo"] = alojamiento.Tipo;
+            ViewData["alojamiento_precioPorPersona"] = alojamiento.PrecioPorPersona;
+            ViewData["alojamiento_precioPorDia"] = alojamiento.PrecioPorDia;
+            ViewData["alojamiento_habitaciones"] = alojamiento.Habitaciones;
+            ViewData["alojamiento_banios"] = alojamiento.Banios;
+
+            return View();
+        }
+
+        // POST: Reservas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(DateTime fechaDesde, DateTime fechaHasta, int id_alojamiento)
+        {
+            if (!this.DisponibilidadPorFechas(id_alojamiento, fechaDesde, fechaHasta))
+            {
+                _soundPlayer = new SoundPlayer("Resources/ErrorSound.wav");
+                _soundPlayer.Play();
+                return Redirect("/Reservas/Create?id=" + User.Identity.Name + "&message=Las-fechas-seleccionadas-no-estan-disponibles");
+            }
+
+            var usuario = this._context.Usuario.Find(int.Parse(User.Identity.Name));
+            var alojamiento = await this._context.Alojamiento.FindAsync(id_alojamiento);
+            int dias_reservados = (fechaHasta - fechaDesde).Days;
+
+            double precio = dias_reservados * alojamiento.PrecioPorDia;
+            if (alojamiento.Tipo == "hotel")
+                precio = dias_reservados * alojamiento.CantidadDePersonas * alojamiento.PrecioPorPersona;
+
+            var reserva = new Reserva
+            {
+                FechaDesde = fechaDesde,
+                FechaHasta = fechaHasta,
+                Alojamiento = alojamiento,
+                Precio = precio,
+                Usuario = usuario
+            };
+
+            this._context.Reserva.Add(reserva);
+            this._context.SaveChanges();
+            _soundPlayer = new SoundPlayer("Resources/SuccessSound.wav");
+            _soundPlayer.Play();
+            return Redirect("/Alojamientoes/all");
+        }
+
+        // GET: Reservas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reserva = await _context.Reserva.FindAsync(id);
+            if (reserva == null)
+            {
+                return NotFound();
+            }
+            return View(reserva);
+        }
+
+        // POST: Reservas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FechaDesde,FechaHasta,Precio")] Reserva reserva)
+        {
+            if (id != reserva.Id)
+            {
+                _soundPlayer = new SoundPlayer("Resources/ErrorSound.wav");
+                _soundPlayer.Play();
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(reserva);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReservaExists(reserva.Id))
+                    {
+                        _soundPlayer = new SoundPlayer("Resources/ErrorSound.wav");
+                        _soundPlayer.Play();
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                _soundPlayer = new SoundPlayer("Resources/SuccessSound.wav");
+                _soundPlayer.Play();
+                return RedirectToAction(nameof(Index));
+            }
+            _soundPlayer = new SoundPlayer("Resources/ErrorSound.wav");
+            _soundPlayer.Play();
+            return View(reserva);
+        }
+
+        // GET: Reservas/Delete/5
+        public async Task<IActionResult> Delete(int? id, String? message)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reserva = await _context.Reserva
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (reserva == null)
+            {
+                return NotFound();
+            }
+
+            return View(reserva);
+        }
+
+        // POST: Reservas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var reserva = await _context.Reserva.FindAsync(id);
+            _context.Reserva.Remove(reserva);
+            await _context.SaveChangesAsync();
+            _soundPlayer = new SoundPlayer("Resources/DeleteSound.wav");
+            _soundPlayer.Play();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReservaExists(int id)
+        {
+            return _context.Reserva.Any(e => e.Id == id);
+        }
+
+        private bool DisponibilidadPorFechas(int id_alojamiento, DateTime fechaDesde, DateTime fechaHasta)
+        {
+            foreach (var reserva in this._context.Reserva.Where(r => r.Alojamiento.Id == id_alojamiento).ToList())
+            {
+                bool validarFechaDesde = DateTime.Compare(reserva.FechaDesde, fechaDesde) == 1 && DateTime.Compare(reserva.FechaDesde, fechaHasta) == 1;
+                bool validarFechaHasta = DateTime.Compare(reserva.FechaHasta, fechaDesde) == -1 && DateTime.Compare(reserva.FechaHasta, fechaDesde) == -1;
+                if (!validarFechaDesde && !validarFechaHasta)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
